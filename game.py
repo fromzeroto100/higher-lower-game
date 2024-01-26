29ecacbcea61d7e14c6824c36c715de8ee80dc0c
Shokhrukh Tukhtasinov <shoxruxbekbest@gmail.com>
2024-01-26 22:20:22 +0100

    last

--- game.py
+++ game.py
@@ -28,13 +28,17 @@ user_choice = ""
 def format_data(account):
     account_name = account["name"]
     account_descr = account["description"]
-    account_country = account[""]
+    account_country = account["country"]
+    return f"{account_name}, a {account_descr}, from {account_country}"
 # Generate a random account from the game
 account_a = random.choice(data)
 account_b = random.choice(data)
 
 if account_a == account_b:
     account_b = random.choice(data)
+print(f"Compare A: {format_data(account_a)}") 
+print(vs)   
+print(f"Compare B: {format_data(account_b)}") 
   
 # Format the account data into printable format
 
