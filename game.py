2c6470d0b46a160163e5fb7b38c444581af65e48
Shokhrukh Tukhtasinov <shoxruxbekbest@gmail.com>
2024-01-26 21:43:46 +0100

    new1

--- game.py
+++ game.py
@@ -2,29 +2,47 @@ from art import logo
 from art import vs
 from game_data import data
 import random
-
+# Display art
 print(logo)
 
 
-game_end = False
 
-user_choice = ""
-
-# function for random choice
-def people_data(profile):
-    print("name: ", profile.get("name", "N/A"))
-    print("description: ", profile.get("description", "N/A"))
-    print("country: ", profile.get("country", "N/A"))
+# def people_data(profile):
+#     print("name: ", profile.get("name", "N/A"))
+#     print("description: ", profile.get("description", "N/A"))
+#     print("country: ", profile.get("country", "N/A"))
   
 
-random_people = random.sample(data, 2) 
+# random_people = random.sample(data, 2) 
+ 
 
-for person in random_people:
-    people_data(person)   
-    print(vs)
+# for person in random_people:
+#     people_data(person)   
+#     print(vs)
 
-user_choice = input("Enter your choice: ")    
-def compare_answer():
-    if user_choice["name:"]
+# Generate a random account from the game
+def get_random_account():
+    return random.choice(data)
 
-#function for comparison
+# Format the account data into printable format
+def format_data(account):
+    name = account["name"]
+    description = account["description"]
+    country = account["country"]
+
+    return(f"{name}: a {description}, from {country}")
+# Ask user for a guess
+
+# Check if user is correct.
+
+# Get follower count of each account
+
+# Use if statement to check if user is correct
+
+# Give user feedback on their guess 
+
+# Score keeping
+
+# Making account at position B become next account at position A
+
+# Clear the screen between rounds
