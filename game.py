a1082cbfd95f11fce486ad93b9c7014cdc2f5ad8
Shokhrukh Tukhtasinov <shoxruxbekbest@gmail.com>
2024-01-28 13:02:38 +0100

    new1

--- game.py
+++ game.py
@@ -8,8 +8,8 @@ print(logo)
 
 game_end = False
 
-user_choice = ""
-
+score = 0
+game_should_continue = True
 
 # def people_data(profile):
 #     print("name: ", profile.get("name", "N/A"))
@@ -30,16 +30,37 @@ def format_data(account):
     account_descr = account["description"]
     account_country = account["country"]
     return f"{account_name}, a {account_descr}, from {account_country}"
-# Generate a random account from the game
-account_a = random.choice(data)
-account_b = random.choice(data)
 
-if account_a == account_b:
+def check_answer(guess, a_followers, b_followers):
+    if a_followers > b_followers:
+        return guess == "a"
+    else:
+        return guess == "b"
+# Generate a random account from the game
     account_b = random.choice(data)
-print(f"Compare A: {format_data(account_a)}") 
-print(vs)   
-print(f"Compare B: {format_data(account_b)}") 
-  
+
+while game_should_continue:
+    account_a = account_b
+    account_b = random.choice(data)
+
+    while account_a == account_b:
+        account_b = random.choice(data)
+    print(f"Compare A: {format_data(account_a)}") 
+    print(vs)   
+    print(f"Compare B: {format_data(account_b)}") 
+
+    guess = input("Who has more followers? Type 'A' or 'B': ").lower
+
+    a_follower_count = account_a["follower_count"]
+    b_follower_count = account_b["follower_count"] 
+    is_correct = check_answer(guess, a_follower_count, b_follower_count) 
+
+    if is_correct:
+        score += 1
+        print(f"You are right! Current score: {score}")
+    else:
+        game_should_continue = False
+        print(f"Sorry you are wrong. Final score: {score}")    
 # Format the account data into printable format
 
 # Ask user for a guess
